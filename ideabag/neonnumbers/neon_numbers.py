400a44738e60c4e36944c847aced4b0e348cda72
Rens Oliemans <rens.oliemans@gmail.com>
2017-04-27 11:32:39 +0200

    finished neon numbers

--- ideabag/neonnumbers/neon_numbers.py
+++ ideabag/neonnumbers/neon_numbers.py
@@ -1,5 +1,13 @@
 def is_neon_number(number):
     squared = pow(number, 2)
     total = 0
-    for int(digit in str(squared):
+    for digit in str(squared):
         total += int(digit)
+    return total == number
+
+def find_neon_numbers(start=1, end=1000):
+    numbers = list()
+    for i in range(start, end):
+        if is_neon_number(i):
+            numbers.append(i)
+    return numbers
