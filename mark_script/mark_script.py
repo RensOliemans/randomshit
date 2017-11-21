15cd46f38c87766acf267880e58832cccaf662ff
Rens Oliemans <rens.oliemans@gmail.com>
2017-11-21 21:04:32 +0100

    Now adds .mp3 after .mp3s again

--- mark_script/mark_script.py
+++ mark_script/mark_script.py
@@ -7,6 +7,8 @@ print('This program changes the ID3 tags of all .mp3 files '
       'in this directory.\n' +
       'The files have to have the format "<song name> - <song artist>.mp3"\n')
 mp3files = [f for f in os.listdir() if f[-4:] == '.mp3']
+features = ['featuring', 'feat.', 'ft.', 'FT.', 'FEAT.', 'feat', 'ft', 'FT',
+            'FEAT', 'Feat.', 'Feat']
 incorrect_files = 0
 for f in mp3files:
     # format of filename: <song_name> - <song_artist>.mp3 (including space)
@@ -19,11 +21,27 @@ for f in mp3files:
         continue
 
     artist = text[0]
-    title = text[1][:-4]
+    rest = text[1][:-4]
+    title = None
+    for feature_text in features:
+        if feature_text in rest:
+            # todo: get featured artist from title and append to artist
+            splitted = rest.split(feature_text)
+            title = splitted[0]
+            featured_artist = splitted[1]
+            artist += ' ft.' + featured_artist
+            break
+
+    if not title:
+        title = rest
 
     audiofile = eyed3.load(f)
-    audiofile.tag.artist, audiofile.tag.title = artist, title
+    try:
+        audiofile.tag.artist, audiofile.tag.title = artist, title
+    except:
+        print(f)
     audiofile.tag.save()
+    os.rename(f, title + ".mp3")
 
 if incorrect_files:
     print("There were {} incorrectly named files".format(incorrect_files))
