11ed6710cfd187a92675d275e1ceddd73dca817a
Rens Oliemans <rens.oliemans@gmail.com>
2017-12-04 20:48:56 +0100

    Works with fractions

--- dobbel/dice.py
+++ dobbel/dice.py
@@ -1,38 +1,55 @@
+"""
+This module represents a dice object. The dice can have any amount of faces
+and can be weighted or not.
+"""
 from numpy.random import choice
+from fractions import Fraction
 
 
 class Dice(object):
 
     def __init__(self, faces=[1, 2, 3, 4, 5, 6], weight=None):
         if weight is None:
+            # set weight to be [1,1,1,...] (fair dice)
             weight = [1 for _ in range(len(faces))]
         self.faces = faces
         if len(weight) != len(faces):
             raise ValueError("Length of weight must be the same as the amount of faces!")
-        # save given weight, since that is more readable than the normalised weight
-        self.readable_weight = weight
-        self.weight = weight
-        self.is_fair = all([x == self.weight[0] for x in self.weight])
+
+        self.weight = self.determine_integer_weight(weight)
+        # fair if all weights are the same
+        self.is_fair = all([x == weight[0] for x in weight])
         # normalise the weights
-        sum_weight = sum(self.weight)
-        for i, val in enumerate(self.weight):
-            self.weight[i] = val / sum_weight
+        sum_weight = sum(weight)
+        self.normalised_weight = self.weight
+        for i, val in enumerate(weight):
+            self.normalised_weight[i] = val / sum_weight
+
+    def determine_integer_weight(self, weight):
+        fractions = [Fraction(x).limit_denominator() for x in weight]
+        highest_denominator = max([x.denominator for x in fractions])
+        # round instead of int - incorrect conversion from float to fraction and vice
+        # versa might lead to something like 56.9999999999 - which should be 57 instead
+        # of 56, which is what would happen if int() would do the job
+        return [round(x * highest_denominator) for x in weight]
 
     def roll(self, times=1):
         rolls = list()
         for _ in range(times):
-            rolls.append(choice(self.faces, p=self.weight))
+            rolls.append(choice(self.faces, p=self.normalised_weight))
         return rolls
-
-    def is_fair(self):
-        return all([x == self.weight[0] for x in self.weight])
 
     def get_weight(self):
         return self.weight
 
+    def change_faces(self, faces=None):
+        if faces and len(faces) == len(self.weight):
+            self.faces = faces
+        else:
+            raise ValueError("Amount of faces must be the same as the length of weights!")
+
     def change_weight(self, weight=None):
-        if len(weight) == len(self.faces):
-            self.readable_weight = weight
+        if weight and len(weight) == len(self.faces):
             sum_weight = sum(weight)
             for i, val in enumerate(weight):
                 self.weight[i] = val / sum_weight
@@ -44,3 +61,6 @@ class Dice(object):
 
     def __repr__(self):
         return "Dice, faces: {}, weight: {}".format(self.faces, self.weight)
+
+    def __eq__(self, other):
+        return self.faces == other.faces and self.weight == other.weight
