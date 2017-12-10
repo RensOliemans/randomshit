87ff18744f42aed1fc7957ae115cf2ea99051a3d
Rens Oliemans <rens.oliemans@gmail.com>
2017-12-10 22:37:03 +0100

    Cleaned mark script

--- mark_script/mark_script.py
+++ mark_script/mark_script.py
@@ -9,7 +9,7 @@ print('This program changes the ID3 tags of all .mp3 files '
 mp3files = [f for f in os.listdir() if f[-4:] == '.mp3']
 features = ['featuring', 'feat.', 'ft.', 'FT.', 'FEAT.', 'feat', 'ft', 'FT',
             'FEAT', 'Feat.', 'Feat']
-incorrect_files = 0
+incorrect_files = list()
 for f in mp3files:
     # format of filename: <song_name> - <song_artist>.mp3 (including space)
     text = f.split(' - ')
@@ -17,20 +17,20 @@ for f in mp3files:
         # incorrect format, skip this file
         print("Tag of file '{}' won't be changed, the format is incorrect"
               .format(f))
-        incorrect_files += 1
+        incorrect_files.append(f)
         continue
 
-    artist = text[0]
-    rest = text[1][:-4]
+    artist = text[0]  # <song_name>
+    rest = text[1][:-4]  # <song_artist>, without .mp3
     title = None
-    for feature_text in features:
-        if feature_text in rest:
-            # todo: get featured artist from title and append to artist
-            splitted = rest.split(feature_text)
-            title = splitted[0]
-            featured_artist = splitted[1]
-            artist += ' ft.' + featured_artist
-            break
+    # determine if one of the feature texts is in the song artist part
+    has_feature = any([feature in rest for feature in features])
+    if has_feature:
+        feature_text = [x for x in features if x in rest][0]  # get feature text
+        splitted = rest.split(feature_text)
+        title = splitted[0]
+        featured_artist = splitted[1]
+        artist += ' ft.' + featured_artist
 
     if not title:
         title = rest
@@ -38,12 +38,17 @@ for f in mp3files:
     audiofile = eyed3.load(f)
     try:
         audiofile.tag.artist, audiofile.tag.title = artist, title
-    except:
-        print(f)
-    audiofile.tag.save()
-    os.rename(f, title + ".mp3")
+        audiofile.tag.save()
+        os.rename(f, title + ".mp3")
+    except eyed3.Error:
+        # unknown why, this happens when eye3d throws an error (audiofile.tag
+        # is None, not sure what makes this happen)
+        print("Error in file {}".format(f))
+        incorrect_files.append(f)
 
 if incorrect_files:
-    print("There were {} incorrectly named files".format(incorrect_files))
+    print("There were {} incorrectly named files:\n{}"
+          .format(len(incorrect_files), incorrect_files))
+
 print("Changed tags of {} files. It took {:.3} seconds."
       .format(len(mp3files) - incorrect_files, time.time() - start))
