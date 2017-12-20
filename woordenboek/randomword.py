72ad8156174474f85cb009337e4c523d0887f523
Rens Oliemans <rens.oliemans@gmail.com>
2017-12-20 18:06:54 +0100

    Code cleanup

--- woordenboek/randomword.py
+++ woordenboek/randomword.py
@@ -3,49 +3,39 @@ import os
 import sys
 import getopt
 
-from random import shuffle
+from random import sample
 
-NL = "nl_NL.dic"
 EN = "en_EN.dic"
 DIR = "dicts/"
 
-
-def get_word(lang=NL, amount_of_words=5, length_of_words=10):
-    f = io.open(DIR + lang, encoding='latin-1')
+def get_word(filename=EN, amount_of_words=5, length_of_words=10):
     words = list()
-    for word in f:
-        word = word[:-1]
-        if "/" in word:
-            index = len(word) - word.index("/")
-            word = word[:-index]
-        words.append(word)
-    f.close()
-    word = ""
-    # we only want long words
-    correct_length_words = [x for x in words if len(x) == length_of_words]
-    shuffle(correct_length_words)
+    with open(DIR + filename) as f:
+        words = [x[:-1] for x in f if len(x) == length_of_words]
+    # f = open(DIR + lang)
+    # f.close()
+
+    amount = min(len(words), amount_of_words)
     print("Amount of words with length {}: {}\n".format(
-          length_of_words, len(correct_length_words)))
-    return correct_length_words[:amount_of_words]
+          length_of_words, amount))
+    return sample(words, min(len(words), amount_of_words))
 
 
 def parse_arguments(argv):
     length_of_words = 10
     amount_of_words = 5
-    lang = EN
+    filename = EN
 
     program_file = os.path.basename(__file__)
     usage_string = "Usage: python3 {} " \
                    "-l <length_of_words> " \
-                   "-a <amount_of_words> " \
-                   "-t <language>".format(program_file)
+                   "-a <amount_of_words> ".format(program_file)
 
     help_string = "<length_of_words> is the amount of characters in a word\n" \
-                  "<amount_of_words> means the amount of words printed\n" \
-                  "<language> is the language of the words, NL or EN."
+                  "<amount_of_words> means the amount of words printed\n"
 
     try:
-        opts, args = getopt.getopt(argv, "ht:l:a:")
+        opts, args = getopt.getopt(argv, "hl:a:")
     except getopt.GetoptError as e:
         print(usage_string)
         sys.exit(2)
@@ -67,18 +57,13 @@ def parse_arguments(argv):
             except ValueError:
                 print("amount_of_words has to be a number")
                 sys.exit(2)
-        elif opt == "-t":
-            if arg == "NL":
-                lang = NL
-            else:
-                lang = EN
-    return length_of_words, amount_of_words, lang
+    return length_of_words, amount_of_words, filename
 
 
 if __name__ == "__main__":
-    length_of_words, amount_of_words, lang = parse_arguments(sys.argv[1:])
+    length_of_words, amount_of_words, filename = parse_arguments(sys.argv[1:])
     words = get_word(
-            lang=lang,
+            filename=filename,
             length_of_words=length_of_words,
             amount_of_words=amount_of_words)
     for word in words:
