84aaef9a3b5d5e129e51c25ae4fed82d9f3e8b39
Rens Oliemans <rens.oliemans@gmail.com>
2018-03-29 15:56:01 +0200

    added rust files

---
+++ rust/guessing_game/target/debug/deps/guessing_game-4664feca438ed99a.d
@@ -0,0 +1,5 @@
+/home/rens/Projects/randomshit/rust/guessing_game/target/debug/deps/guessing_game-4664feca438ed99a: src/main.rs
+
+/home/rens/Projects/randomshit/rust/guessing_game/target/debug/deps/guessing_game-4664feca438ed99a.d: src/main.rs
+
+src/main.rs:
