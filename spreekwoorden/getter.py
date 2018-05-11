c997ac030213ee0a36568274aa12568602326ad2
Rens Oliemans <rens.oliemans@gmail.com>
2018-05-11 12:10:32 +0200

    added spreekwoorden getter

---
+++ spreekwoorden/getter.py
@@ -0,0 +1,20 @@
+from urllib import request
+from bs4 import BeautifulSoup
+import json
+
+url = 'https://spreekwoorden.nl/spreekwoorden-a-z'
+pages = ['?page={}'.format(x) for x in range(1, 681)]
+
+proverbs = dict()
+
+for page in pages:
+    html_page = request.urlopen(url + page).read()
+    soup = BeautifulSoup(html_page, 'html.parser')
+    html_proverbs = soup.find_all(attrs={'class': 'proverb-item'})
+    for proverb in html_proverbs:
+        title = proverb.find(attrs={'class': 'proverb-item__heading'}).text.strip()
+        meaning = proverb.find(attrs={'class': 'proverb-item__body'}).text.strip()
+        proverbs[title] = meaning
+
+with open('spreekwoorden.txt', 'w') as f:
+    json.dump(proverbs, f)
