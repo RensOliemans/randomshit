f5a66357efea833af50d5ea3147d5caa614afd41
Rens Oliemans <rens.oliemans@gmail.com>
2018-05-11 12:45:04 +0200

    neater getter

--- spreekwoorden/getter.py
+++ spreekwoorden/getter.py
@@ -2,19 +2,42 @@ from urllib import request
 from bs4 import BeautifulSoup
 import json
 
-url = 'https://spreekwoorden.nl/spreekwoorden-a-z'
-pages = ['?page={}'.format(x) for x in range(1, 681)]
 
-proverbs = dict()
+URL = 'https://spreekwoorden.nl/spreekwoorden-a-z'
 
-for page in pages:
-    html_page = request.urlopen(url + page).read()
+
+def determine_max_pages(url):
+    html_page = request.urlopen(url).read()
     soup = BeautifulSoup(html_page, 'html.parser')
-    html_proverbs = soup.find_all(attrs={'class': 'proverb-item'})
-    for proverb in html_proverbs:
-        title = proverb.find(attrs={'class': 'proverb-item__heading'}).text.strip()
-        meaning = proverb.find(attrs={'class': 'proverb-item__body'}).text.strip()
-        proverbs[title] = meaning
+    page_list = soup.find(attrs={'class': 'pagination'})
+    # -1 is \n, -2 is next page, -3 is '\n', -4 is last page
+    last = list(page_list.children)[-4]
+    return int(last.text)
 
-with open('dump.json', 'w') as f:
-    json.dump(proverbs, f)
+
+def get_proverbs(url, pages):
+    proverbs = dict()
+    for page in pages:
+        html_page = request.urlopen(url + page).read()
+        soup = BeautifulSoup(html_page, 'html.parser')
+        html_proverbs = soup.find_all(attrs={'class': 'proverb-item'})
+        for proverb in html_proverbs:
+            title = proverb.find(attrs={'class': 'proverb-item__heading'}).text.strip()
+            meaning = proverb.find(attrs={'class': 'proverb-item__body'}).text.strip()
+            proverbs[title] = meaning
+    return proverbs
+
+
+def main():
+    max_pages = determine_max_pages(URL)
+    print(max_pages)
+    pages = ['?page={}'.format(x) for x in range(1, max_pages + 1)]
+
+    proverbs = get_proverbs(URL, pages)
+
+    with open('dump.json', 'w') as f:
+        json.dump(proverbs, f)
+
+
+if __name__ == '__main__':
+    main()
