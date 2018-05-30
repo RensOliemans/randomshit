b2db3fa55e78076c59a32e47550e81818cdefbcd
Rens Oliemans <rens.oliemans@gmail.com>
2018-05-30 17:34:45 +0200

    changed attributes to english

--- pandora/gebouwen/gebouw.py
+++ pandora/gebouwen/gebouw.py
@@ -1,53 +1,52 @@
 class Gebouw:
 
-    def __init__(self, nummer, naam, afkorting, kunstwerk=False):
-        self.nummer = nummer
-        self.naam = naam
-        self.afkorting = afkorting
-        self.kunstwerk = kunstwerk
+    def __init__(self, number, name, abbreviation):
+        self.number = number
+        self.name = name
+        self.abbreviation = abbreviation
 
     def __eq__(self, other):
         if type(other) == type(self):
-            return self.nummer == other.nummer
+            return self.number == other.number
         if type(other) == int:
-            return self.nummer == other
+            return self.number == other
         if type(other) == str:
-            return self.naam == other or self.afkorting == other
+            return self.name == other or self.abbreviation == other
         return False
 
     def __str__(self):
-        return self.naam
+        return self.name
 
     def __repr__(self):
-        return "{} - {} ({})".format(self.nummer, self.naam, self.afkorting)
+        return "{} - {} ({})".format(self.number, self.name, self.abbreviation)
 
     def __contains__(self, x):
-        return x in self.naam.lower()
+        return x in self.name.lower()
 
     def __len__(self):
-        return len(self.naam)
+        return len(self.name)
 
 
 class Kunst:
 
-    def __init__(self, naam):
-        self.naam = naam
+    def __init__(self, name):
+        self.name = name
 
     def __eq__(self, other):
         if type(other) == type(self):
-            return self.naam == other.naam
+            return self.name == other.name
         if type(other) == str:
-            return self.naam == other
+            return self.name == other
         return False
 
     def __str__(self):
-        return self.naam
+        return self.name
 
     def __repr__(self):
         return str(self)
 
     def __contains__(self, x):
-        return x in self.naam.lower()
+        return x in self.name.lower()
 
     def __len__(self):
-        return len(self.naam)
+        return len(self.name)
--- pandora/gebouwen/gebouwen.py
+++ pandora/gebouwen/gebouwen.py
@@ -4,8 +4,17 @@ from gebouw import Gebouw, Kunst
 
 parser = argparse.ArgumentParser(description='Pak alle gebouwen (en kunstwerken) op de UT.')
 parser.add_argument('-l', '--letters', type=str,
-                    help='welke letters heeft het gebouw?')
-parser.add_argument('-k', '--kunst', dest=
+                    help='welke letters heeft het gebouw? (ergens in de naam)')
+parser.add_argument('-a', '--aantal', type=int,
+                    help='hoeveel letters heeft het gebouw?')
+parser.add_argument('-b', '--begin', type=str,
+                    help='wat zijn de eerste letters van het gebouw?')
+parser.add_argument('-e', '--eind', type=str,
+                    help='wat zijn de laatste letters van het gebouw?')
+parser.add_argument('-m', '--midden', type=str,
+                    help='gebruik: [<index><letter> ]+')
+parser.add_argument('-k', '--kunst', dest='kunst', action='store_true',
+                    help='neem de kunstwerken ook mee')
 args = parser.parse_args()
 
 
@@ -16,7 +25,7 @@ def parse_new():
     gebouwen = list()
     for line in lines:
         items = line.split('\t')
-        gebouwen.append(Gebouw(nummer=int(items[0]), naam=items[1], afkorting=items[2]))
+        gebouwen.append(Gebouw(number=int(items[0]), name=items[1], abbreviation=items[2]))
 
     with open('kunst') as f:
         lines = f.read()
@@ -24,10 +33,24 @@ def parse_new():
     kunstwerken = list()
     for line in lines:
         items = line.split('\t')
-        kunstwerken.append(Kunst(naam=items[0]))
+        kunstwerken.append(Kunst(name=items[0]))
 
     return gebouwen, kunstwerken
 
-
 gebouwen, kunstwerken = parse_new()
-print([x for x in gebouwen if all([l in x.naam.lower() for l in args.letters.lower()])])
+total = gebouwen + kunstwerken
+check = total if args.kunst else gebouwen
+
+
+if args.begin:
+    check = [x for x in check if x.name.lower()[:len(args.begin)] == args.begin.lower()]
+if args.midden:
+    indices = {int(x[0]): x[1] for x in args.midden.split(' ')}
+    check = [x for x in check if all(x.name.lower()[min(len(x) - 1, p)] == indices[p] for p in indices.keys())]
+if args.eind:
+    check = [x for x in check if x.name.lower()[-len(letters):] == letters.lower()]
+if args.letters:
+    check = [x for x in check if all([l in x.name.lower() for l in args.letters.lower()])]
+if args.aantal:
+    check = [x for x in check if len(x.name) == args.aantal]
+print(check)
