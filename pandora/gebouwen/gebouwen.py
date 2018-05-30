18ab3eeb77f9f488a5512ed5c5f96939faa1f7cb
Rens Oliemans <rens.oliemans@gmail.com>
2018-05-30 17:55:26 +0200

    oeps added gebouwen.py in uitleg

--- pandora/gebouwen/gebouwen.py
+++ pandora/gebouwen/gebouwen.py
@@ -1,9 +1,9 @@
 '''
 Laat de gebouwen (en kunstwerken) op campus zien die je wilt zien. Er zijn een aantal argumenten die je kan meegeven, met -h zie je die.
 Ze kunnen ook samen gebruikt worden. Een paar voorbeelden daarvan:
-    `python3 -ka 7 -m '1e 2e'`  # laat kunstwerken & gebouwen zien, lengte 7, waar letters van index 1 en 2 'e' zijn
-    `python3 -b vl -e er`       # laat gebouwen zien die beginnen met 'vl' en eindigen met 'er'
-    `python3 -ka 7 -l er`       # laat kunstwerken & gebouwen zien, lengte 7, met letters 'er' ergens in de naam.
+    `python3 gebouwen.py -ka 7 -m '1e 2e'`  # laat kunstwerken & gebouwen zien, lengte 7, waar letters van index 1 en 2 'e' zijn
+    `python3 gebouwen.py -b vl -e er`       # laat gebouwen zien die beginnen met 'vl' en eindigen met 'er'
+    `python3 gebouwen.py -ka 7 -l er`       # laat kunstwerken & gebouwen zien, lengte 7, met letters 'er' ergens in de naam.
 
 
 Van een aantal kunstwerken zijn de varianten met en zonder spatie erin gezet, zoals HetDing.
