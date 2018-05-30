4d3b97f7b113e7f71e7aeb64009be13d6631165d
Rens Oliemans <rens.oliemans@gmail.com>
2018-05-30 17:50:45 +0200

    added explanation

--- pandora/gebouwen/gebouwen.py
+++ pandora/gebouwen/gebouwen.py
@@ -1,3 +1,20 @@
+'''
+Laat de gebouwen (en kunstwerken) op campus zien die je wilt zien. Er zijn een aantal
+argumenten die je kan meegeven, met -h zie je die.
+Ze kunnen ook samen gebruikt worden. Een paar voorbeelden daarvan:
+    `python3 -ka 7 -m '1e 2e'`  # laat kunstwerken & gebouwen zien, lengte 7, waar letters van index 1 en 2 'e' zijn
+    `python3 -b vl -e er`   # laat gebouwen zien die beginnen met 'vl' en eindigen met 'er'
+    `python3 -ka 7 -l er`   # laat kunstwerken & gebouwen zien, lengte 7, met letters 'er' ergens in de naam.
+
+
+Van een aantal kunstwerken zijn de varianten met en zonder spatie erin gezet, zoals HetDing.
+Op een puzzel zou namelijk best wel 'hetding' kunnen komen, maar ook 'het ding'.
+
+Alle straatnamen staan er ook in, als kunstwerken.
+
+Alles is hoofdletterongevoelig, dus `-b wa` pakt Waaier.
+
+'''
 import argparse
 
 from gebouw import Gebouw, Kunst
