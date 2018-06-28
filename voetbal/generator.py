97e1037ee63b6b3ca1ab3c14c0dc612dd098c7fd
Rens Oliemans <rens.oliemans@gmail.com>
2018-06-28 10:43:52 +0200

    Added functionality for average rounds in generator

--- voetbal/generator.py
+++ voetbal/generator.py
@@ -7,7 +7,7 @@ from numpy.random import normal
 
 def main(home, away, counter=0):
     diff = away - home
-    goal_difference = round(normal(loc=diff*2.08, scale=0))
+    goal_difference = round(normal(loc=diff*2.08, scale=0.1))
     if goal_difference == 0:
         goal_difference += choice([-1, 1])
     goal_base = max(round(abs(-math.log2(random() + 0.7))), 0)
@@ -22,7 +22,14 @@ if __name__ == '__main__':
                         help='Chance of home team winning')
     parser.add_argument('-ca', '--chance_away', type=float,
                         help='Chance of away team winning')
+    parser.add_argument('-r', '--rounds', type=int,
+                        help='Does multiple rounds and returns the average score',
+                        default=1)
     args = parser.parse_args()
 
-    result = main(args.chance_home, args.chance_away)
-    print(result)
+    total = [0, 0]
+    for _ in range(args.rounds):
+        result = main(args.chance_home, args.chance_away)
+        total[0] = total[0] + result[0]
+        total[1] = total[1] + result[1]
+    print(f"({total[0] / args.rounds}, {total[1] / args.rounds})")
