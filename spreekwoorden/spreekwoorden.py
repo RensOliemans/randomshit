09fec0ad83aa970ed172c6cf7b6a2aaac34a29e4
Rens Oliemans <rens.oliemans@gmail.com>
2018-07-17 17:52:14 +0200

    :heavy_minus_sign: removed begins dependency for spreekwoorden since it's used throughout the system

--- spreekwoorden/spreekwoorden.py
+++ spreekwoorden/spreekwoorden.py
@@ -1,23 +1,36 @@
 import json
 import os
+import argparse
 from random import sample
 
-import begin
+
+parser = argparse.ArgumentParser(description="Gets some Dutch proverbs at random.")
 
 
-current_dir = os.path.dirname(os.path.realpath(__file__))
-filename = current_dir + '/dump.json'
-proverbs = dict()
-with open(filename) as f:
-    proverbs = json.load(f)
+def load_proverbs():
+    current_dir = os.path.dirname(os.path.realpath(__file__))
+    filename = current_dir + '/dump.json'
+    proverbs = dict()
+    with open(filename) as f:
+        proverbs = json.load(f)
+    return proverbs
 
 
-@begin.start(auto_convert=True)
 def main(a: 'Amount of proverbs' = 5, d: 'To define' = True):
     """ Gets some Dutch proverbs at random. """
+    proverbs = load_proverbs()
     chosen = sample(proverbs.items(), a)
     if d:
         max_len = max([len(elem[0]) for elem in chosen]) + 4
         print('\n'.join(f'{elem[0]:{max_len}}{elem[1]}' for elem in chosen))
     else:
         print('\n'.join(choice[0] for choice in chosen))
+
+
+if __name__ == '__main__':
+    parser.add_argument('-a', '--amount', type=int,
+                        help='Amount of proverbs', default=5)
+    parser.add_argument('-d', '--define', type=bool,
+                        help='To define', default=True)
+    args = parser.parse_args()
+    main(args.amount, args.define)
