93fcc4f1c99c2555c4223d7c4c93badfe964e3ea
Rens Oliemans <rens.oliemans@gmail.com>
2018-11-27 14:56:27 +0100

    :ambulance: Fix snapple with absolute path

--- snapple/snapple.py
+++ snapple/snapple.py
@@ -1,9 +1,9 @@
 import json
-from random import sample
+from random import choice
 
-FILENAME = 'all_facts.json'
+FILENAME = '/home/rens/Projects/randomshit/snapple/all_facts.json'
 
 fact_json = json.load(open(FILENAME))
 
 facts = [fact_json[x]['d'] for x in fact_json]
-print(sample(list(facts), 1))
+print(choice(facts))
