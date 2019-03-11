73cfdf9ce8359ed8af11ea225b3ce4609d26b0bd
Rens Oliemans <rens.oliemans@gmail.com>
2019-03-11 23:55:41 +0100

    :art: Clean code

---
+++ pandora/gebouwen/buildings.py
@@ -0,0 +1,45 @@
+class Item():
+    def __init__(self, name):
+        self.name = name
+
+    def __eq__(self, other):
+        if type(other) == type(self):
+            return self.name == other.name
+        return False
+
+    def __str__(self):
+        return self.name
+
+    def __repr__(self):
+        return f"<Item: {self.name}>"
+
+    def __contains__(self, x):
+        return x in self.name.lower()
+
+    def __len__(self):
+        return len(self.name)
+
+
+class Building(Item):
+
+    def __init__(self, number, name, abbreviation):
+        self.number = number
+        self.name = name
+        self.abbreviation = abbreviation
+
+    def __eq__(self, other):
+        if type(other) == type(self):
+            return self.number == other.number
+        if type(other) == int:
+            return self.number == other
+        if type(other) == str:
+            return self.name == other or self.abbreviation == other
+        return False
+
+    def __repr__(self):
+        return f"<Building: {self.number} - {self.name} ({self.abbreviation})>"
+
+
+class Artwork(Item):
+    def __repr__(self):
+        return f"<Artwork: {self.name}>"
--- pandora/gebouwen/gebouwen.py
+++ pandora/gebouwen/gebouwen.py
@@ -1,9 +1,15 @@
 """
-Laat de gebouwen (en kunstwerken) op campus zien die je wilt zien. Er zijn een aantal argumenten die je kan meegeven, met -h zie je die.
+Laat de gebouwen (en kunstwerken) op campus zien die je wilt zien.
+Er zijn een aantal argumenten die je kan meegeven, met -h zie je die.
 Ze kunnen ook samen gebruikt worden. Een paar voorbeelden daarvan:
-    `python3 gebouwen.py -ka 7 -m '1e 2e'`  # laat kunstwerken & gebouwen zien, lengte 7, waar letters van index 1 en 2 'e' zijn
-    `python3 gebouwen.py -b vl -e er`       # laat gebouwen zien die beginnen met 'vl' en eindigen met 'er'
-    `python3 gebouwen.py -ka 7 -l er`       # laat kunstwerken & gebouwen zien, lengte 7, met letters 'er' ergens in de naam.
+    # laat kunstwerken & gebouwen zien, lengte 7, waar letters van index 1 en 2 'e' zijn
+    `python3 gebouwen.py -ka 7 -m '1e 2e'`
+
+    # laat gebouwen zien die beginnen met 'vl' en eindigen met 'er'
+    `python3 gebouwen.py -b vl -e er`
+
+    # laat kunstwerken & gebouwen zien, lengte 7, met letters 'er' ergens in de naam.
+    `python3 gebouwen.py -ka 7 -l er`
 
 
 Van een aantal kunstwerken zijn de varianten met en zonder spatie erin gezet, zoals HetDing.
@@ -16,54 +22,106 @@ Alles is hoofdletterongevoelig, dus `-b wa` pakt Waaier.
 ''"""
 import argparse
 
-from gebouw import Gebouw, Kunst
+from buildings import Building, Artwork
 
-parser = argparse.ArgumentParser(description='Pak alle gebouwen (en kunstwerken) op de UT.')
+
+parser = argparse.ArgumentParser(description='Get all buildings (and artworks) on the UT.')
 parser.add_argument('-l', '--letters', type=str,
-                    help='welke letters heeft het gebouw? (ergens in de naam)')
-parser.add_argument('-a', '--aantal', type=int,
-                    help='hoeveel letters heeft het gebouw?')
+                    help='what letters does the building have (somewhere in the name)')
+parser.add_argument('-a', '--amount', type=int,
+                    help='how many letters does the building have')
 parser.add_argument('-b', '--begin', type=str,
-                    help='wat zijn de eerste letters van het gebouw?')
-parser.add_argument('-e', '--eind', type=str,
-                    help='wat zijn de laatste letters van het gebouw?')
-parser.add_argument('-m', '--midden', type=str,
-                    help='gebruik: [<index><letter> ]+')
-parser.add_argument('-k', '--kunst', dest='kunst', action='store_true',
-                    help='neem de kunstwerken ook mee')
-args = parser.parse_args()
+                    help='what are the first letters of the building?')
+parser.add_argument('-e', '--end', type=str,
+                    help='what are the last letters of the building?')
+parser.add_argument('-m', '--middle', type=str,
+                    help='use: [<index><letter> ]+')
+parser.add_argument('-w', '--work-of-art', dest='include_artworks', action='store_true',
+                    help='take the works of art')
 
 
-def parse_new():
-    # File has multiple lines, every line belonging to one building, and the
-    # individual information parts are put into segments split by '\t'
-    # for every line in the text file, split the line into segments separated
-    # by '\t', and these are the numbers, names and abbreviations
-    gebouwen = [Gebouw(int(x[0][0]), x[0][1], x[0][2]) for x in
-                zip([[item for item in line.split('\t')]
-                    for line in open('buildings.txt').read().split('\n')[1:-1]])]
+def main():
+    buildings, artworks = get_buildings_and_artworks('buildings.txt', 'artworks.txt')
 
-    kunstwerken = [Kunst(x[0][0]) for x in
-                   zip([[item for item in line.split('\t')]
-                       for line in open('kunst.txt').read().split('\n')[1:-1]])]
+    filters = parser.parse_args()
+    filtered_items = filter_items(buildings, artworks, filters)
 
-    return gebouwen, kunstwerken
+    print(filtered_items)
+
+
+def get_buildings_and_artworks(buildings_filename, artworks_filename):
+    lines = get_lines_from_file(buildings_filename)
+    buildings = parse_buildings_from_lines(lines)
+
+    lines = get_lines_from_file(artworks_filename)
+    artworks = parse_artworks_from_lines(lines)
+
+    return buildings, artworks
+
+
+def get_lines_from_file(filename, building=True):
+    with open(filename) as f:
+        lines = f.read().split('\n')[1:-1]
+        return lines
+
+
+def parse_buildings_from_lines(lines):
+    for line in lines:
+        columns = line.split('\t')
+        number, name, abbreviation = columns[0], columns[1], columns[2]
+        yield Building(int(number), name, abbreviation)
+
+
+def parse_artworks_from_lines(lines):
+    for line in lines:
+        columns = line.split('\t')
+        name = columns[0]
+        yield Artwork(name)
+
+
+def filter_items(buildings, artworks, filters):
+    if filters.include_artworks:
+        filtered_items = apply_filters(buildings + artworks, filters)
+    else:
+        filtered_items = apply_filters(buildings, filters)
+    return filtered_items
+
+
+def apply_filters(items, filters):
+    filtered_items = items
+    if filters.begin:
+        filtered_items = apply_begin_filter(filtered_items, filters.begin)
+    if filters.middle:
+        filtered_items = apply_middle_filter(filtered_items, filters.middle)
+    if filters.end:
+        filtered_items = apply_end_filter(filtered_items, filters.end)
+    if filters.letters:
+        filtered_items = apply_some_letters_filter(filtered_items, filters.letters)
+    if filters.amount:
+        filtered_items = apply_amount_filter(filtered_items, filters.amount)
+    return filtered_items
+
+
+def apply_begin_filter(items, first_letters):
+    return [x for x in items if x.name.lower()[:len(first_letters)] == first_letters.lower()]
+
+
+def apply_middle_filter(items, middle_letters):
+    indices = {int(x[0]): x[1] for x in middle_letters.split(' ')}
+    return [x for x in items if all(x.name.lower()[min(len(x) - 1, p)] == indices[p] for p in indices.keys())]
+
+
+def apply_end_filter(items, last_letters):
+    return [x for x in items if x.name.lower()[-len(last_letters):] == last_letters.lower()]
+
+
+def apply_some_letters_filter(items, some_letters):
+    return [x for x in items if all([l in x.name.lower() for l in some_letters.lower()])]
+
+
+def apply_amount_filter(items, amount_of_letters):
+    return [x for x in items if len(x.name) == amount_of_letters]
 
 
 if __name__ == '__main__':
-    gebouwen, kunstwerken = parse_new()
-    total = gebouwen + kunstwerken
-    check = total if args.kunst else gebouwen
-
-    if args.begin:
-        check = [x for x in check if x.name.lower()[:len(args.begin)] == args.begin.lower()]
-    if args.midden:
-        indices = {int(x[0]): x[1] for x in args.midden.split(' ')}
-        check = [x for x in check if all(x.name.lower()[min(len(x) - 1, p)] == indices[p] for p in indices.keys())]
-    if args.eind:
-        check = [x for x in check if x.name.lower()[-len(args.eind):] == args.eind.lower()]
-    if args.letters:
-        check = [x for x in check if all([l in x.name.lower() for l in args.letters.lower()])]
-    if args.aantal:
-        check = [x for x in check if len(x.name) == args.aantal]
-    print(check)
+    main()
