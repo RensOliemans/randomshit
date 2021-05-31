ecba4af0e08f1b2d5d25656b838fc4767183ca30
Rens Oliemans <hallo@rensoliemans.nl>
2021-05-31 16:23:08 +0200

    :newspaper: Add grolsch ek

---
+++ grolsch-ek/game.py
@@ -0,0 +1,28 @@
+from random import SystemRandom
+
+r = SystemRandom()
+
+
+class Game:
+    def __init__(self, people):
+        self.people = people
+        assert len(people) > 0, "Has to be a sequence of at least length 1"
+
+    def roll(self):
+        number = r.uniform(0.0, 60.0)
+        return sorted(self.people, key=lambda person: abs(number - person.guess))[0]
+
+
+class Person:
+    def __init__(self):
+        self.guess = r.uniform(0.0, 60.0)
+
+
+class DirtyA(Person):
+    def __repr__(self):
+        return f'<DirtyA. Guess: {self.guess}>'
+
+
+class Other(Person):
+    def __repr__(self):
+        return f'<Other. Guess: {self.guess}>'
