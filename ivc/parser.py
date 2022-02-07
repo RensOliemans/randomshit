75341fe4652a4640c97f75bb6f577eb929ee133b
Rens Oliemans <hallo@rensoliemans.nl>
2022-02-07 14:35:15 +0100

    Show boxes around all string close to inter-actief

--- ivc/parser.py
+++ ivc/parser.py
@@ -1,11 +1,15 @@
 import cv2
 import pytesseract
 from pytesseract import Output
+from Levenshtein import distance
 
 FILENAME = 'index.jpg'
 OUTPUT = 'output.jpg'
 TEAM = 'Inter-Actief'
+
 PIXELWIDTH = 5
+DIST = 3
+EXPECTED_APPEARANCES = 7
 
 
 def parse_image(filename):
@@ -13,14 +17,27 @@ def parse_image(filename):
     return img, pytesseract.image_to_data(img, output_type=Output.DICT)
 
 
-def highlight_boxes(img, data, text, pixelwidth):
+def find_boxes(image, data, team, pixelwidth, dist, expected):
+    n = highlight_boxes(image, d, team, pixelwidth, dist)
+    while n < expected:
+        dist += 1
+        n = highlight_boxes(image, d, team, pixelwidth, dist)
+
+
+def highlight_boxes(img, data, text, pixelwidth, dist=5):
+    n = 0
     for i, box in enumerate(data['text']):
-        if box == text:
+        if same(box, text, dist):
             (x, y, w, h) = (data['left'][i] - pixelwidth, data['top'][i] - pixelwidth,
                             data['width'][i] + 2 * pixelwidth, data['height'][i] + 2 * pixelwidth)
             img = cv2.rectangle(img, (x, y), (x + w, y + h), (0, 0, 255), 2)
+            n += 1
 
-    return img
+    return n
+
+
+def same(found, actual, dist):
+    return distance(found.lower(), actual.lower()) < dist
 
 
 def output(img, filename=None):
@@ -32,6 +49,6 @@ def output(img, filename=None):
 
 
 if __name__ == '__main__':
-    img, data = parse_image(FILENAME)
-    img = highlight_boxes(img, data, TEAM, PIXELWIDTH)
-    output(img, OUTPUT)
+    image, d = parse_image(FILENAME)
+    find_boxes(image, d, TEAM, PIXELWIDTH, DIST, EXPECTED_APPEARANCES)
+    output(image, OUTPUT)
