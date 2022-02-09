c637b4ea49264c3720412247438e4f97cd7535c2
Rens Oliemans <hallo@rensoliemans.nl>
2022-02-09 12:23:47 +0100

    Add more options to script
    You can copy/paste using pyperclip, this was chosen since it's a
    lightweight module that should work across platforms. Only tested on
    Linux.

--- diceware/phrase.py
+++ diceware/phrase.py
@@ -1,4 +1,5 @@
 import argparse
+import pyperclip
 from pathlib import Path
 from random import SystemRandom
 
@@ -14,9 +15,29 @@ parser.add_argument(
     help="numbers to get diceware words from",
 )
 parser.add_argument(
-    "--filename", default="composites-nl", help="filename of the diceware list"
+    "-f", "--filename", default="composites-nl", help="filename of the diceware list"
 )
-parser.add_argument("--length", default=6, type=int, help="length of the passphrase")
+parser.add_argument(
+    "-l", "--length", default=6, type=int, help="length of the passphrase"
+)
+parser.add_argument(
+    "-c",
+    "--copy",
+    default=False,
+    action=argparse.BooleanOptionalAction,
+    help="Copy the passphrase to the clipboard",
+)
+parser.add_argument(
+    "-s",
+    "--show",
+    default=True,
+    action=argparse.BooleanOptionalAction,
+    help="Print the passphrase to stdout",
+)
+
+
+def get_file(filename):
+    return Path(__file__).parent.absolute().joinpath(filename)
 
 
 def get_words(filename):
@@ -34,16 +55,23 @@ def generate_pw(words, length):
         x += 1
 
 
+def output_passphrase(passphrase, args):
+    if args.show:
+        print(passphrase)
+    if args.copy:
+        pyperclip.copy(passphrase)
+
+
 def main(args):
-    f = Path(__file__).parent.absolute().joinpath(args.filename)
+    f = get_file(args.filename)
     words = get_words(f)
     if args.numbers:
         try:
-            print(" ".join(words[int(n)] for n in args.numbers))
+            output_passphrase(" ".join(words[int(n)] for n in args.numbers), args)
         except KeyError:
             print("Number does not exist")
     else:
-        print(" ".join(generate_pw(words, args.length)))
+        output_passphrase(" ".join(generate_pw(words, args.length)), args)
 
 
 if __name__ == "__main__":
